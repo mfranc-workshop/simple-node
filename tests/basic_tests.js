c6bb93c4dc287ed68bf380826bc175f7ba29b3c7
michal-franc <lam.michal.franc@gmail.com>
2016-11-13 23:15:18 +0000

    adapting unit tests to jest

--- tests/basic_tests.js
+++ tests/basic_tests.js
@@ -1,9 +1,9 @@
-
-var assert = require('chai').assert;
 var supertest = require('supertest');
 var serviceLib = require('../service'), service = serviceLib.service;
 
 var serviceName = 'basic-service-test';
+
+var now = (done) => (err, res) => err ? done.fail(err) : done();
 
 var sut = new service(serviceName);
 
@@ -21,14 +21,23 @@ describe('micro-service', function() {
   });
 
   it('returns 200 on /status', function(done) {
-    request.get('/status').expect(200, done);
+    request
+    .get('/status')
+    .expect(200)
+    .end(now(done));
   });
 
   it('returns json', function(done) {
-    request.get('/status').expect('Content-Type', /application\/json/).end(done);
+    request
+    .get('/status')
+    .expect('Content-Type', /application\/json/)
+    .end(now(done));
   });
 
   it('returns name of the service', function(done) {
-    request.get('/status').expect({name: serviceName}).end(done);
+    request
+    .get('/status')
+    .expect({name: serviceName})
+    .end(now(done));
   });
 });
